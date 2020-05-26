4facc4d06f5d0229546a190edbf3a9892437b719
Kode <kodestar@gmail.com>
2020-05-26 21:44:45 +0100

    Integrate jsqr

--- src/views/Send.vue
+++ src/views/Send.vue
@@ -10,7 +10,8 @@
       <button class="scan btn outline" @click="scanQR" type="button">Scan QR</button>
     <div id="scan" class="page" :class="{active: scan !== false}">
         <a class="close" @click="closeScan"><i class="fal fa-times"></i></a>
-        <video id="qrpreview"></video>
+        <div v-if="loadingMsg" id="loadingMessage">{{ loadingMsg }}</div>
+        <canvas id="canvas" hidden></canvas>
     </div>
 
   </div>
@@ -20,7 +21,9 @@
 import { serverMixin } from '../mixins/serverMixin.js'
 import * as NanoCurrency from 'nanocurrency'
 import BigNumber from 'bignumber.js'
-import Instascan from 'instascan'
+import jsQR from 'jsqr'
+
+var video = document.createElement("video")
 
 export default {
   name: 'Send',
@@ -33,7 +36,10 @@ export default {
       amount: '',
       destination: '',
       scan: false,
-      scanner: null
+      scanner: null,
+      canvas: null,
+      stream: null,
+      loadingMsg: 'Unable to access video stream (please make sure you have a webcam enabled)',
     }
   },
   computed: {
@@ -55,7 +61,7 @@ export default {
       }
       const amount = NanoCurrency.convert(this.amount, this.nanoconv)
       const senderpublickey = NanoCurrency.derivePublicKey(this.privatekey)
-      const sender = NanoCurrency.deriveAddress(senderpublickey,{useNanoPrefix:true}); 
+      const sender = NanoCurrency.deriveAddress(senderpublickey,{useNanoPrefix:true})
       let info = {}
       info['action'] = 'account_info'
       info['representative'] = 'true'
@@ -68,7 +74,7 @@ export default {
         representative: res.representative,
         balance: balance,
         link: this.destination
-      });
+      })
       console.log(block)
       let send = {}
       send['action'] = 'process'
@@ -80,30 +86,63 @@ export default {
       this.$emit('close', 'true')
     },
     closeScan () {
+      video.srcObject.getTracks().forEach(function(track) {
+        track.stop()
+      })
       this.scan = false
-      this.scanner.stop()
+
     },
     async scanQR () {
       this.scan = true
-      this.scanner = new Instascan.Scanner({ video: document.getElementById('qrpreview') })
-      this.scanner.addListener('scan', function (content) {
-        that.destination = content.replace('nano:','')
-        that.closeScan()
+
+
+      var canvasElement = document.getElementById("canvas")
+      this.canvas = canvasElement.getContext("2d")
+
+      const that = this
+
+      // Use facingMode: environment to attemt to get the front camera on phones
+      navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
+        video.srcObject = stream
+        video.setAttribute("playsinline", true) // required to tell iOS safari we don't want fullscreen
+        video.play()
+        requestAnimationFrame(tick)
       })
-      let that = this
-      Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-          console.log('starting first camera')
-          that.scanner.start(cameras[0])
-        } else {
-          console.error('No cameras found.')
+
+      function tick() {
+        that.loadingMsg = "Loading video..."
+        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+          that.loadingMsg = null
+          canvasElement.hidden = false
+
+          canvasElement.height = video.videoHeight
+          canvasElement.width = video.videoWidth
+          that.canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height)
+          var imageData = that.canvas.getImageData(0, 0, canvasElement.width, canvasElement.height)
+          var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert",
+          })
+          if (code) {
+            video.srcObject.getTracks().forEach(function(track) {
+              track.stop()
+            })
+            that.destination = code.data.replace('nano:','')
+            that.scan = false
+            return true
+          }
         }
-      }).catch(function (e) {
-        console.error(e)
-      })
+        if(that.scan === false) return
+        requestAnimationFrame(tick)
+      }
 
     }
   }
 
 }
 </script>
+<style lang="scss" scoped>
+#canvas {
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+}
+</style>
