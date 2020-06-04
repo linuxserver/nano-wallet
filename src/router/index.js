7e8683e644d690da6be41f5c16b716e454354778
KodeStar <kodestar@gmail.com>
2020-06-04 09:56:17 +0100

    move about page

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,14 @@ Vue.use(VueRouter)
     component: Generate
   },
   {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
     path: '/block/:hash',
     name: 'Block',
     component: Block
@@ -49,14 +57,6 @@ Vue.use(VueRouter)
     path: '/:node/address/:address',
     name: 'Address',
     component: Address
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
