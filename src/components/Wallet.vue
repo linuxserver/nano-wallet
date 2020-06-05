13180a2684eddb97463474fdeb87fbaadaf10f4f
Kode <kodestar@gmail.com>
2020-06-05 22:19:53 +0100

    fixes

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="wallet">
-    <button class="btn outline" @click="createWallet" type="button">Paper Wallet</button>
-    <div class="page" style="z-index: 9; justtify-content: flex-start;" :class="{active: wallet !== false}">
-      <div class="inner">
+    <button style="width: 100%;" class="btn outline" @click="createWallet" type="button">Paper Wallet</button>
+    <div class="page" style="z-index: 9; top: 0; bottom: 0; justtify-content: flex-start;" :class="{active: wallet !== false}">
+      <div class="fdcol">
       <a class="close" @click="closeWallet"><i class="fal fa-times"></i></a>
-      <canvas width="2480" height="1508" style="background: white; max-height: 360px; height: auto;" :id="'wallet_' + _uid"></canvas>
-      <div id="private"></div>
-      <div id="public"></div>
-      <button @click="printWallet">Print</button>
+      <div class="printpos">
+        <canvas width="2480" height="3508" :id="'wallet_' + _uid"></canvas>
+        <div class="posbuttons">
+          <button class="btn" :class="{ outline: position !== 1}" @click="position = 1">Position 1</button>
+          <button class="btn" :class="{ outline: position !== 2}" @click="position = 2">Position 2</button>
+          <button class="btn" :class="{ outline: position !== 3}" @click="position = 3">Position 3</button>
+        </div>
+      </div>
+      <button style="width: 100%; margin-top: 20px;" class="btn" @click="printWallet">Print</button>
     </div>
     </div>
   </div>
 </template>
 
 <script>
-import QRCodeStyling from 'qr-code-styling'
+// import QRCodeStyling from 'qr-code-styling'
+import QRCode from 'qrcode'
 
 export default {
   name: 'Wallet',
@@ -25,9 +31,31 @@ export default {
   data() {
     return {
       wallet: false,
+      position: 1,
+      src: null,
+      loaded: 0,
+      canvas: null
     }
   },
-
+  watch: {
+    position: function (newpos, oldpos) {
+      if (newpos !== oldpos) {
+        this.loaded = 0
+        this.createWallet()
+      }
+    },
+    loaded: function (newpos) {
+      if (newpos === 4) {
+        this.src = this.canvas.toDataURL("image/png")
+      }
+    }
+  },
+  computed: {
+    startTop () {
+      if (this.position === 1) return parseInt(0)
+      return parseInt((this.position - 1) * 1169)
+    }
+  },
   methods: {
     closeWallet () {
       this.wallet = false
@@ -35,69 +63,175 @@ export default {
     },
     async createWallet () {
       this.wallet = true
-      const canvasElement = document.getElementById('wallet_' + this._uid)
-      const canvas = canvasElement.getContext("2d")
+      this.canvas = document.getElementById('wallet_' + this._uid)
+      const ctx = this.canvas.getContext("2d")
 
-      canvas.width = 2480
-      canvas.height = 1508
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      ctx.width = 2480
+      ctx.height = 3508
+
+      const that = this
 
       const base_image = new Image()
       base_image.src = '/wallet.png'
       base_image.onload = function(){
-        canvas.drawImage(base_image, 0, 0);
+        that.loaded += 1
+        ctx.drawImage(base_image, 0, parseInt(that.startTop) + 0)
       }
 
       const sectionwidth = 828
       const sectionheight = 585
 
-      const that = this
 
-      const qrCode = new QRCodeStyling({
-        width: 512,
-        height: 512,
-        data: that.private,
-        image: 'qr_logo.png',
-        dotsOptions: {
-          type: 'rounded'
-        },
-        imageOptions: {
-          hideBackgroundDots: false
-        },
-        qrOptions: {
-          //errorCorrectionLevel: 'H'
-        }
-      })
       // qrCode.append(document.getElementById('private'));
 
-      console.log(qrCode._canvas)
-      console.log(qrCode._canvas.getCanvas())
-      const privateSrc = qrCode._canvas.getCanvas()
+      // Private
+      const privateSrc = await QRCode.toDataURL(this.private)
+
+      ctx.font = "50px Nunito"
+      ctx.fillText("S E E D", sectionwidth + 40, this.startTop + 100)
+
+      console.log(this.private)
+
+      let text1 = this.private.substring(0, 8)
+      let text2 = this.private.substring(8, 16)
+      let text3 = this.private.substring(16, 24)
+      let text4 = this.private.substring(24, 32)
+      let text5 = this.private.substring(32, 40)
+      let text6 = this.private.substring(40, 48)
+      let text7 = this.private.substring(48, 56)
+      let text8 = this.private.substring(56, 64)
+
+      ctx.font = "38px Monospace"
+
+      ctx.fillText(text1.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 160)
+      ctx.fillText(text2.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 200)
+      ctx.fillText(text3.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 240)
+      ctx.fillText(text4.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 280)
+      ctx.fillText(text5.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 320)
+      ctx.fillText(text6.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 360)
+      ctx.fillText(text7.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 400)
+      ctx.fillText(text8.split("").join(String.fromCharCode(8202)), sectionwidth + 40, this.startTop + 440)
+
       const privateQr = new Image()
       privateQr.onload = function () {
-        canvas.drawImage(privateQr, sectionwidth, 0);
-        canvas.strokeRect(sectionwidth, 0, privateQr.width, privateQr.height);
+        that.loaded += 1
+        let y = (sectionheight - 400) / 2
+        ctx.drawImage(privateQr, sectionwidth + 400, that.startTop + y, 400, 400)
       }
-      // console.log(document.getElementById('private').firstElementChild.toDataURL())
+      privateQr.src = privateSrc
+
+      // End private
+
+      // Address
+      const publicSrc = await QRCode.toDataURL(this.public)
+
+      ctx.font = "50px Nunito"
+      ctx.fillText("A D D R E S S", (sectionwidth * 2) + 40, this.startTop + sectionheight + 100)
+
+      console.log(this.public)
+
+      text1 = this.public.substring(0, 8)
+      text2 = this.public.substring(8, 16)
+      text3 = this.public.substring(16, 24)
+      text4 = this.public.substring(24, 32)
+      text5 = this.public.substring(32, 40)
+      text6 = this.public.substring(40, 48)
+      text7 = this.public.substring(48, 56)
+      text8 = this.public.substring(56, 64)
+
+      ctx.font = "38px Monospace"
+      ctx.fillStyle='#d43789'
+      ctx.fillText(text1.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 160)
+      ctx.fillStyle='#000000'
+      ctx.fillText(text2.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 200)
+      ctx.fillText(text3.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 240)
+      ctx.fillText(text4.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 280)
+      ctx.fillText(text5.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 320)
+      ctx.fillText(text6.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 360)
+      ctx.fillText(text7.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 400)
+      ctx.fillStyle='#d43789'
+      ctx.fillText(text8.split("").join(String.fromCharCode(8202)), (sectionwidth * 2) + 40, this.startTop + sectionheight + 440)
+      
+      const publicQr = new Image()
+      publicQr.onload = function () {
+        that.loaded += 1
+        let y = ((sectionheight - 400) / 2) + sectionheight
+        ctx.drawImage(publicQr, (sectionwidth * 2) + 400, that.startTop + y, 400, 400)
+      }
+      publicQr.src = publicSrc
+
+      // End Address
+
+
+
+// console.log(document.getElementById('private').firstElementChild.toDataURL())
       // privateQr.src = document.getElementById('private').firstElementChild.toDataURL()
 
       console.log(privateSrc)
 
       const logo_image = new Image()
       logo_image.onload = function(){
+        that.loaded += 1
         let x = (sectionwidth - logo_image.width) / 2
-        let y = (sectionheight - logo_image.height) / 2
+        let y = that.startTop + ((sectionheight - logo_image.height) / 2)
 
-        canvas.drawImage(logo_image, x, sectionheight + y);
+        ctx.drawImage(logo_image, x, sectionheight + y)
       }
       logo_image.src = '/wallet_logo.png'
 
 
     },
     async printWallet () {
-      console.log('print')
+      var win = window.open('about:blank', "_new");
+    win.document.open();
+    win.document.write([
+        '<html>',
+        '   <head>',
+        '   </head>',
+        '   <body onload="window.print()" onafterprint="window.close()">',
+        '       <img style="width:100%" src="' + this.src + '"/>',
+        '   </body>',
+        '</html>'
+    ].join(''));
+    win.document.close();
     }
 
   }
 }
 </script>
-
+<style lang="scss">
+.posbuttons {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+.printpos {
+  display: flex;
+  canvas {
+    background: white; 
+    max-width: 200px; 
+    height: auto;
+    margin-right: 20px;
+    border-radius: 20px;
+  }
+}
+.fdcol {
+  flex-direction: column;
+}
+@media print {
+  .wallet {
+    canvas {
+      max-width: 100%!important;
+    }
+    .close {
+      display: none;
+    }
+    > * {
+      padding: 0;
+      margin: 0;
+    }
+  }
+}
+</style>
--- src/views/Generate.vue
+++ src/views/Generate.vue
@@ -77,6 +77,7 @@ export default {
 <style lang="scss" scoped>
 .btn {
   width: 100%;
+  margin-bottom: 30px;
 }
 .canvas-bag {
   text-align: center;
