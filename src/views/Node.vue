a3250db389f8be9085abab988f6c19707fffc425
Kode <kodestar@gmail.com>
2020-06-15 18:03:16 +0100

    Update Node.vue

--- src/views/Node.vue
+++ src/views/Node.vue
@@ -1,9 +1,13 @@
 <template>
   <div style="top: 0;" id="blockdetails" class="page active">
-    <label for="key">Enter node address</label>
+    <div id="inputs">
+          <div class="labeltabs">
+          <label class="active" for="key">Enter node address</label>
+          </div>
     <input v-model="node" type="text" id="node" name="node">
     <button @click="openNode" class="openwallet btn" type="button">Go To Node</button>
-    <p style="margin-top: 40px;">This wallet is designed to use any Nano Node RPC server as a backend, using nano.linuxserver.io will plug you into our own public network.</p><p>If you are looking for the live Nano network please use <a class="highlight" href="https://tixwallet.cc/" target="_blank">tixwallet.cc</a>.</p><p>More information including how to get funds from a faucet is available on <a class="highlight" target="_blank" href="https://github.com/linuxserver/nano-wallet/">GitHub</a>.</p>
+          </div>
+    <p style="margin-top: 40px;">This wallet is designed to use any Nano Node RPC server as a backend, using nano.linuxserver.io will plug you into our own public network. More information including how to get funds from a faucet is available on <a class="highlight" target="_blank" href="https://github.com/linuxserver/nano-wallet/">GitHub</a>.</p><p>If you are looking for the live Nano network please use <a class="highlight" href="https://tixwallet.cc/" target="_blank">tixwallet.cc</a>.</p>
     <p><a class="highlight" target="_blank" href="https://github.com/linuxserver/nano-wallet/"><i class="fab fa-2x fa-github"></i></a></p>
   </div>
 </template>
