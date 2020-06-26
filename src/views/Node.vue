1843347fac3af95a9caaf4eeb2282529ac50dc46
thelamer <ryankuba@gmail.com>
2020-06-26 08:58:26 -0700

    add button to quickly go to localhost port 7076

--- src/views/Node.vue
+++ src/views/Node.vue
@@ -13,6 +13,7 @@
       <input v-if="showadvanced === true" v-model="address" placeholder="https://domain.com:443/path" type="text" id="address" name="address">
       <input v-if="showadvanced === true" v-model="auth" placeholder="Auth Header *optional" type="text" id="auth" name="auth">
       <button v-if="showadvanced === false" @click="openNode" class="openwallet btn" type="button">Go To Node</button>
+      <button v-if="showadvanced === false" @click="openLocalhost" class="openwallet btn" type="button">Local Node</button>
       <button v-if="showadvanced === true" @click="openAdvanced" class="openwallet btn" type="button">Go To Node</button>
     </div>
     <p style="margin-top: 40px;">This wallet is designed to use any Nano Node RPC server as a backend, using nano.linuxserver.io will plug you into our own public network. More information including how to get funds from a faucet is available on <a class="highlight" target="_blank" href="https://github.com/linuxserver/nano-wallet/">GitHub</a>.</p><p>If you are looking for the live Nano network please use <a class="highlight" href="https://tixwallet.cc/" target="_blank">tixwallet.cc</a>.</p>
@@ -79,6 +80,9 @@ export default {
           type: 'error'
         })
       }
+    },
+    openLocalhost () {
+      this.$router.push('/127.0.0.1')
     },
     setAdvanced () {
       this.showadvanced = true
